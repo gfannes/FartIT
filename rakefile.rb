4211af1ecd45a5e0b71ed306dfb462be2f31538d
Geert Fannes <geert.fannes@gmail.com>
2018-05-30 22:00:26 +0200

    updated the rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -2,12 +2,9 @@ require(File.join(ENV['gubg'], 'shared'))
 include GUBG
 
 task :define do
-    require('gubg/build/Executable')
-    exe = Build::Executable.new('fart')
-    exe.add_include_path('Fart')
-    exe.set_cache_dir('.cache')
-    exe.add_sources(FileList.new('Fart/*.c*'))
-    exe.add_sources(FileList.new('Fart/*.h'))
-    exe.build
-    publish(exe.exe_filename, dst: 'bin')
+    publish("fart.exe", dst: 'bin') do |fn|
+        case os
+        when :windows then fn
+        else fn.gsub(/\.exe$/, "") end
+    end
 end
